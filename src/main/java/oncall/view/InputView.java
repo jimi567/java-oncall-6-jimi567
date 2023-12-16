9689371762d7a0d678d794557b43e62e2000edb1
KB Kim <28584160+jimi567@users.noreply.github.com>
2023-12-16 14:08:50 +0900

    feat(InputView): 입력 클래스 구현

---
+++ src/main/java/oncall/view/InputView.java
@@ -0,0 +1,31 @@
+package oncall.view;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class InputView {
+
+    private static final InputView INSTANCE = new OutputView();
+    private final OutputView outputView = OutputView.getInstance();
+
+    private InputView() {
+    }
+
+    public static InputView getInstance() {
+        return INSTANCE;
+    }
+
+    public String readDate() {
+        outputView.EmergencyWorkDatePrompt();
+        return Console.readLine();
+    }
+
+    public String readWeekDayOrder() {
+        outputView.WeekDayEmergencyNamePrompt();
+        return Console.readLine();
+    }
+
+    public String readWeekendDayOrder() {
+        outputView.WeekendDayEmergencyNamePrompt();
+        return Console.readLine();
+    }
+}
