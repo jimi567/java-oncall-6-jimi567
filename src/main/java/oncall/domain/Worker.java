65aa19c4350361d0893b597ec63dfb619c3ed415
KB Kim <28584160+jimi567@users.noreply.github.com>
2023-12-16 14:20:25 +0900

    feat(Worker): 근무자 도메인 유효성 검증 기능 까지 구현

---
+++ src/main/java/oncall/domain/Worker.java
@@ -0,0 +1,30 @@
+package oncall.domain;
+
+import static oncall.consts.Error.INVALID_MESSAGE;
+
+public class Worker {
+
+    private static final int MAXIMUM_NAME_SIZE = 5;
+    private final String name;
+
+    Worker(String name) {
+        validate(name);
+        this.name = name;
+    }
+
+    private void validate(String name) {
+        if (!validateSize(name)) {
+            INVALID_MESSAGE.throwException();
+        }
+    }
+
+    private boolean validateSize(String name) {
+        return name.length() <= MAXIMUM_NAME_SIZE;
+    }
+
+    @Override
+    public boolean equals(Object name) {
+        return this.name.equals(name);
+    }
+
+}
