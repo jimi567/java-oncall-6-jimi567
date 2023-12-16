94a3b6a910ff48f2fb51bc59e3a4ef659b890274
KB Kim <28584160+jimi567@users.noreply.github.com>
2023-12-16 14:10:18 +0900

    feat(Error) : 예외처리 담당하는 Enum 구현

---
+++ src/main/java/oncall/consts/Error.java
@@ -0,0 +1,21 @@
+package oncall.consts;
+
+public enum Error {
+
+    INVALID_MESSAGE("유효하지 않은 입력 값입니다. 다시 입력해 주세요."),
+
+    private final String ERROR = "[ERROR] ";
+    private final String message;
+
+    Error(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return ERROR + message;
+    }
+
+    public void throwException(){
+        throw new IllegalArgumentException(getMessage());
+    }
+}
