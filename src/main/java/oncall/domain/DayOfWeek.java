4255f27c9b4874989a74bed25c94d7e8efd8c517
KB Kim <28584160+jimi567@users.noreply.github.com>
2023-12-16 15:14:31 +0900

    feat(DayOfWeek) : 요일 정보를 담는 도메인 추가

---
+++ src/main/java/oncall/domain/DayOfWeek.java
@@ -0,0 +1,27 @@
+package oncall.domain;
+
+public class DayOfWeek {
+    private String dayOfWeek;
+
+    DayOfWeek(String dayOfWeek) {
+        this.dayOfWeek = dayOfWeek;
+    }
+
+    public void updateNextDay() {
+        if (dayOfWeek.equals("월")) {
+            dayOfWeek = "화";
+        } else if (dayOfWeek.equals("화")) {
+            dayOfWeek = "수";
+        } else if (dayOfWeek.equals("수")) {
+            dayOfWeek = "목";
+        } else if (dayOfWeek.equals("목")) {
+            dayOfWeek = "금";
+        } else if (dayOfWeek.equals("금")) {
+            dayOfWeek = "토";
+        } else if (dayOfWeek.equals("토")) {
+            dayOfWeek = "일";
+        } else if (dayOfWeek.equals("일")) {
+            dayOfWeek = "월";
+        }
+    }
+}
