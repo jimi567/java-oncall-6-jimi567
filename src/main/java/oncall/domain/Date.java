789e0210739e9f5374c84f164a5f97ce2742fc53
KB Kim <28584160+jimi567@users.noreply.github.com>
2023-12-16 15:17:09 +0900

    feat(Date) : 날짜 정보를 관리하는 도메인 추가
    날을 업데이트 하는 기능과
    공휴일인지 판단하는 기능

---
+++ src/main/java/oncall/domain/Date.java
@@ -0,0 +1,47 @@
+package oncall.domain;
+
+import static oncall.consts.Calender.getLastDate;
+
+import oncall.consts.Calender;
+
+public class Date {
+    private final int month;
+    private int date;
+
+    public Date(int month, int date) {
+        this.month = month;
+        this.date = date;
+    }
+
+    public boolean updateDay() {
+        if (getLastDate(month) <= date) {
+            return false;
+        }
+        ++date;
+        return true;
+    }
+
+    public boolean isHoliday() {
+        return Calender.isHoliday(this);
+    }
+
+    public int getMonth() {
+        return month;
+    }
+
+    public int getDate() {
+        return date;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null || getClass() != obj.getClass()) {
+            return false;
+        }
+        Date otherDate = (Date) obj;
+        return month == otherDate.month && date == otherDate.date;
+    }
+}
